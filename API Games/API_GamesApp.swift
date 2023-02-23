8e1c18180462a784d036295998e238b9ba300c9c
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-23 09:58:30 -0600

    Initial Commit

---
+++ API Games/API_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_GamesApp.swift
+//  API Games
+//
+//  Created by Quinn Kozak on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct API_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API GamesUITests/API_GamesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  API_GamesUITestsLaunchTests.swift
+//  API GamesUITests
+//
+//  Created by Quinn Kozak on 2/23/23.
+//
+
+import XCTest
+
+final class API_GamesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
