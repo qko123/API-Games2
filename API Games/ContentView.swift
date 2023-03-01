7269846217cace2455a0e81cfa0878adfdbfca47
Quinn Kozak <quinn.kozak@icloud.com>
2023-03-01 10:02:31 -0600

    Got API working

--- API Games/ContentView.swift
+++ API Games/ContentView.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var titles = [Titles]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(titles) { heading in
+                NavigationLink {
+                    VStack {
+                        Text(heading.titles)
+                    }
+                } label: {
+                    Text(heading.titles)
+                }
+            }
+            .navigationTitle("Games By Title")
         }
-        .padding()
+        .task {
+            await getTitles()
+        }
     }
+    func getTitles() async {
+            let query = "https://www.cheapshark.com/api/1.0/deals?storeID=1"
+            if let url = URL(string: query) {
+                if let (data, _) = try? await URLSession.shared.data(from: url) {
+                    if let decodedResponse = try? JSONDecoder().decode([Titles].self, from: data) {
+                        titles = decodedResponse
+                    }
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Titles: Identifiable, Codable {
+    var id = UUID()
+    var titles: String
+    
+    
+    enum CodingKeys: String, CodingKey {
+        case titles = "title"
+        
     }
 }
